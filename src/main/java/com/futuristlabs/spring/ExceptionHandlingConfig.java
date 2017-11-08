d3bef6f1818c2e1e10fc01df02477b67b51a5d87
Gencho Zhilkov <zhilkov@futurist-labs.com>
2017-11-08 14:35:38 +0200

    spring: add SpringBoot validation dep & exception handler.

--- src/main/java/com/futuristlabs/spring/ExceptionHandlingConfig.java
+++ src/main/java/com/futuristlabs/spring/ExceptionHandlingConfig.java
@@ -8,12 +8,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @ControllerAdvice
 public class ExceptionHandlingConfig extends ResponseEntityExceptionHandler {
@@ -21,6 +32,27 @@ public class ExceptionHandlingConfig extends ResponseEntityExceptionHandler {
     @AllArgsConstructor
     private static class ErrorDescription {
         private final String error;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class ValidationError {
+        private final String field;
+        private final String message;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class ValidationErrors {
+        private final List<ValidationError> errors;
+
+        static ValidationErrors from(final BindingResult br) {
+            final List<ValidationError> errors = br.getFieldErrors().stream()
+                    .map(field -> new ValidationError(field.getField(), field.getDefaultMessage()))
+                    .collect(toList());
+
+            return new ValidationErrors(errors);
+        }
     }
 
     private HttpStatus resolveAnnotatedResponseStatus(Exception exception) {
@@ -43,5 +75,23 @@ public class ExceptionHandlingConfig extends ResponseEntityExceptionHandler {
         HttpStatus status = resolveAnnotatedResponseStatus(ex);
         ErrorDescription message = resolveExceptionMessage(ex);
         return new ResponseEntity<>(message, headers, status);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(BindException ex,
+                                                         HttpHeaders headers,
+                                                         HttpStatus status,
+                                                         WebRequest request) {
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(ValidationErrors.from(ex.getBindingResult()), headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(ValidationErrors.from(ex.getBindingResult()), headers, HttpStatus.BAD_REQUEST);
     }
 }
