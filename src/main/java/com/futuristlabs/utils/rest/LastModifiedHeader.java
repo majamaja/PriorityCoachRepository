4eec53470e2ceb0f66744182620eb9007cda4c4e
Gencho Zhilkov <zhilkov@futurist-labs.com>
2018-02-28 00:29:21 +0200

    utils: LastModifiedHeader - add useful methods.

--- src/main/java/com/futuristlabs/utils/rest/LastModifiedHeader.java
+++ src/main/java/com/futuristlabs/utils/rest/LastModifiedHeader.java
@@ -1,11 +1,15 @@
 package com.futuristlabs.utils.rest;
 
+import com.futuristlabs.func.exceptions.NotModifiedException;
+import com.futuristlabs.utils.repository.PageData;
+
 import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class LastModifiedHeader<T> {
     private final LastModifiedExtractor<? super T> extractor;
@@ -27,14 +31,31 @@ public class LastModifiedHeader<T> {
         return entry;
     }
 
-    public <U extends T> List<U> setAndReturn(HttpServletResponse response, List<U> data) {
-        final LocalDateTime lastModified = data
-                .stream()
+    public <U extends T> U checkSetAndReturn(HttpServletResponse response, U entry, LocalDateTime lastModifiedSince) {
+        final LocalDateTime entryLastModified = this.extractor.getLastModified(entry);
+        if (lastModifiedSince != null && entryLastModified != null && entryLastModified.isBefore(lastModifiedSince)) {
+            throw new NotModifiedException();
+        }
+        return setAndReturn(response, entry);
+    }
+
+    public <U extends T> Stream<U> setAndReturn(HttpServletResponse response, Stream<U> stream) {
+        final LocalDateTime lastModified = stream
                 .filter(entry -> this.extractor.getLastModified(entry) != null)
                 .map(this.extractor::getLastModified)
                 .max(LocalDateTime::compareTo)
                 .orElseGet(LocalDateTime::now);
         setLastModified(response, lastModified);
+        return stream;
+    }
+
+    public <U extends T> List<U> setAndReturn(HttpServletResponse response, List<U> data) {
+        setAndReturn(response, data.stream());
         return data;
+    }
+
+    public <U extends T> PageData<U> setAndReturn(HttpServletResponse response, PageData<U> page) {
+        setAndReturn(response, page.getItems().stream());
+        return page;
     }
 }
