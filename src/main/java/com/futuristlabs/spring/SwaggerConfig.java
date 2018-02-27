8f2a6f5de15bf744c19829037ba1da3eb827937f
Gencho Zhilkov <zhilkov@futurist-labs.com>
2018-02-28 00:34:46 +0200

    spring: Swagger enhancements & dep. version update.

--- src/main/java/com/futuristlabs/spring/SwaggerConfig.java
+++ src/main/java/com/futuristlabs/spring/SwaggerConfig.java
@@ -11,24 +11,43 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Date;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
-	public Docket api() {
+	public Docket mobile() {
 		return new Docket(DocumentationType.SWAGGER_2)
-		  .ignoredParameterTypes(AuthenticationPrincipal.class)
-		  .groupName("futuristlabs-template-api")
-		  .select()
-		  .apis(RequestHandlerSelectors.basePackage("com.futuristlabs.rest"))
-		  .paths(PathSelectors.any())
-		  .build()
-		  //according to bug https://github.com/springfox/springfox/issues/1569 ApiKey name should match reference in SecurityReference
-		  .securitySchemes(Collections.singletonList(new ApiKey("X-Auth-Token", "xAuthToken", "header")))
-		  .securityContexts(Collections.singletonList(generateXAuthTokenSecurityContext()))
-		  .apiInfo(new ApiInfo("FuturistLabs Template Project API definitions", "FuturistLabs Template Project API definitions", "1.0", null, (Contact) null, null, null, Collections.emptyList()));
+				.ignoredParameterTypes(AuthenticationPrincipal.class)
+				.groupName("codehospitality-mobile-api")
+				.directModelSubstitute(LocalDateTime.class, Date.class)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.codehospitality.rest.v1_0"))
+				.paths(PathSelectors.any())
+				.build()
+				//according to bug https://github.com/springfox/springfox/issues/1569 ApiKey name should match reference in SecurityReference
+				.securitySchemes(Collections.singletonList(new ApiKey("X-Auth-Token", "xAuthToken", "header")))
+				.securityContexts(Collections.singletonList(generateXAuthTokenSecurityContext()))
+				.apiInfo(new ApiInfo("Code Hospitality MOBILE API definitions", "Code Hospitality MOBILE API definitions", "1.0", null, (Contact) null, null, null, Collections.emptyList()));
+	}
+
+	@Bean
+	public Docket admin() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.ignoredParameterTypes(AuthenticationPrincipal.class)
+				.groupName("codehospitality-admin-api")
+				.directModelSubstitute(LocalDateTime.class, Date.class)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.codehospitality.rest.admin"))
+				.paths(PathSelectors.any())
+				.build()
+				//according to bug https://github.com/springfox/springfox/issues/1569 ApiKey name should match reference in SecurityReference
+				.securitySchemes(Collections.singletonList(new ApiKey("X-Auth-Token", "xAuthToken", "header")))
+				.securityContexts(Collections.singletonList(generateXAuthTokenSecurityContext()))
+				.apiInfo(new ApiInfo("Code Hospitality ADMIN API definitions", "Code Hospitality ADMIN API definitions", "1.0", null, (Contact) null, null, null, Collections.emptyList()));
 	}
 
 	private SecurityContext generateXAuthTokenSecurityContext() {
