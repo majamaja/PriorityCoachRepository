b005dd3faaa95bddbaf328c6d5a24a5bcf4c0e7e
Gencho Zhilkov <zhilkov@futurist-labs.com>
2018-02-28 00:38:58 +0200

    spring: Additional exception handlers.

--- src/main/java/com/futuristlabs/spring/ExceptionHandlingConfig.java
+++ src/main/java/com/futuristlabs/spring/ExceptionHandlingConfig.java
@@ -1,33 +1,51 @@
 package com.futuristlabs.spring;
 
 import com.futuristlabs.func.exceptions.ApplicationException;
+import com.fasterxml.jackson.core.JsonParseException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.ConstraintViolationException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.NoSuchElementException;
 
 import static java.util.stream.Collectors.toList;
 
 @ControllerAdvice
 public class ExceptionHandlingConfig extends ResponseEntityExceptionHandler {
+    private enum Errors {
+        INVALID_JSON(HttpStatus.BAD_REQUEST),
+        NOT_FOUND(HttpStatus.NOT_FOUND),
+        CONSTRAINT_VIOLATION(HttpStatus.CONFLICT),
+        DUPLICATE_DATA(HttpStatus.CONFLICT);
+
+        private HttpStatus status;
+
+        Errors(HttpStatus status) {
+            this.status = status;
+        }
+
+        private ResponseEntity<Object> response(HttpHeaders headers) {
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            return new ResponseEntity<>(new ErrorDescription(name()), headers, status);
+        }
+    }
+
     @Data
     @AllArgsConstructor
     private static class ErrorDescription {
@@ -77,21 +95,39 @@ public class ExceptionHandlingConfig extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(message, headers, status);
     }
 
+    @ExceptionHandler({EmptyResultDataAccessException.class, NoSuchElementException.class})
+    protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
+        return Errors.NOT_FOUND.response(new HttpHeaders());
+    }
+
+    @ExceptionHandler({DuplicateKeyException.class})
+    protected ResponseEntity<Object> handleDuplicateKeyException(DuplicateKeyException ex, WebRequest request) {
+        return Errors.DUPLICATE_DATA.response(new HttpHeaders());
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    protected ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+        return Errors.CONSTRAINT_VIOLATION.response(new HttpHeaders());
+    }
+
     @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex,
-                                                         HttpHeaders headers,
-                                                         HttpStatus status,
-                                                         WebRequest request) {
+    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         headers.setContentType(MediaType.APPLICATION_JSON);
         return new ResponseEntity<>(ValidationErrors.from(ex.getBindingResult()), headers, HttpStatus.BAD_REQUEST);
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers,
-                                                                  HttpStatus status,
-                                                                  WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         headers.setContentType(MediaType.APPLICATION_JSON);
         return new ResponseEntity<>(ValidationErrors.from(ex.getBindingResult()), headers, HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        if (ex.getCause() instanceof JsonParseException) {
+            return Errors.INVALID_JSON.response(headers);
+        } else {
+            return super.handleExceptionInternal(ex, body, headers, status, request);
+        }
     }
 }
