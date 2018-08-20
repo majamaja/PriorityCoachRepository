977dfc363ab9d683d644f59c1ed08546d88b77db
Stef Vartolomeev <stef.vartolomeev@gmail.com>
2018-08-20 17:51:47 +0300

    db: Fix migration script

--- src/main/sql/01 - weekly based tasks.sql
+++ src/main/sql/01 - weekly based tasks.sql
@@ -12,10 +12,18 @@ ALTER TABLE life_upgrade_actions ADD COLUMN times_per_week INT NOT NULL CHECK (t
 
 
 -- Upgrade Actions will track the user commitment
+ALTER TABLE user_actions_log ADD COLUMN life_upgrade_action_id CHAR(36);
+
+UPDATE user_actions_log l
+SET life_upgrade_action_id = (SELECT i.action_id FROM user_action_items i
+WHERE i.id = l.user_action_item_id);
+
+ALTER TABLE user_actions_log MODIFY life_upgrade_action_id CHAR(36) NOT NULL;
+ALTER TABLE user_actions_log ADD CONSTRAINT user_actions_log_life_upgrade_action_id_fk FOREIGN KEY (life_upgrade_action_id) REFERENCES life_upgrade_actions(id);
+
 ALTER TABLE user_actions_log DROP FOREIGN KEY user_actions_log_user_action_item_id_fk;
 ALTER TABLE user_actions_log DROP COLUMN user_action_item_id;
 
-ALTER TABLE user_actions_log ADD COLUMN life_upgrade_action_id CHAR(36) NOT NULL;
-ALTER TABLE user_actions_log ADD CONSTRAINT user_actions_log_life_upgrade_action_id_fk FOREIGN KEY (life_upgrade_action_id) REFERENCES life_upgrade_actions(id);
+
 
 DROP TABLE user_action_items;
